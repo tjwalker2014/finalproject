b57e2d7df539c202c28f362fb6c699ed334543c5
Tom <tj.walker@live.com>
2014-09-12 00:57:28 +0100

    fixed some more js issues

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,21 +3,24 @@ var myApp = myApp || {}
 myApp.setUpEventListeners = function(){
 
   $('.front-card').on('click', function(){
+    
+    var $div_to_rotate = $($(this).data('rotate'));
+    var $div_to_show = $($(this).data('show'));
 
-    var target_id = $(this).data('target');
-
-    var $target_div = $(target_id);
-
-    if (!$target_div.hasClass('rotate')) {
-      $target_div.addClass('rotate');
+    if (!$div_to_rotate.hasClass('rotate')) {
+      $div_to_rotate.addClass('rotate');
       $('.front-card').addClass('avoid-clicks');
+      $div_to_show.addClass('show-div');
     }
 
     $('.close-icon').on('click', function(){
-      $target_div.removeClass('rotate');
+      $div_to_rotate.removeClass('rotate');
       $('.front-card').removeClass('avoid-clicks');
+      $div_to_show.removeClass('show-div');
     });
+
   });
+
 };
 
 $(document).ready(function(){
@@ -30,19 +33,6 @@ $(document).ready(function(){
 
   app.controller('appController' , ["$scope", "$http" ,function($scope, $http) {
         
-    // $http.get("http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?v=2&alt=json").success(function(data) {
-    //     $scope.youtubedata = data;
-    //     // console.log("youtube")
-    //     console.log(data)
-    // })
-
-    // SC.oEmbed("https://soundcloud.com/selkiemusic/dancing-in-the-dark-springsteen-cover", function(oembed){
-    //   // console.log("oEmbed response: ", oembed['html']);
-    //   $('#track').append(oembed['html']);
-    //   var widget = SC.Widget(document.querySelector('#track iframe'));
-
-    //   // console.log(oembed);
-
     //   $('#song').click(function(){
     //     widget.toggle();
     //     widget.getCurrentSound(function(sound) {  
@@ -50,41 +40,37 @@ $(document).ready(function(){
     //     });
     //   });
 
-    //   $('#song-close').click(function(){
-    //     widget.toggle();
-    //   });
+    $scope.newSoundcloudTrack = function() {
 
-    //   $('#next').click(function(){
-    //     widget.next();
-    //   });
-
-    // });
-                                                   
-    SC.initialize({
-      client_id: "f2152c9cb7d7b6a1ab6cb2a93c90f8b7"
-    });
-
-    SC.get('/tracks', function(tracks) {
-      console.log(tracks.length);
-
-      // var arrayLength = tracks.length;
-      tracks.forEach(function(element){
-        console.log(element.id)
-        console.log(element.title)
+      SC.initialize({
+        client_id: "f2152c9cb7d7b6a1ab6cb2a93c90f8b7"
       });
+
+      SC.get('/tracks', function(tracks) {
+        console.log(tracks.length);
+
+        // var arrayLength = tracks.length;
+        tracks.forEach(function(element){
+          console.log(element.id)
+          console.log(element.title)
+        });
+        
+        var random = Math.floor(Math.random() * 49);
+        var track_url = tracks[random].permalink_url;
+        // console.log(random, track_url);
       
-      var random = Math.floor(Math.random() * 49);
-      var track_url = tracks[random].permalink_url;
-      // console.log(random, track_url);
-    
-      SC.oEmbed(track_url, function(oembed){
-        console.log(oembed);
-        $('#track').append(oembed['html']);
-        var widget = SC.Widget(document.querySelector('#track iframe'));
-        console.log(widget)
+        SC.oEmbed(track_url, function(oembed){
+          $('#soundcloud-div').empty()
+          $('#soundcloud-div').append(oembed['html']);
+          test = (oembed['html'])  
+        });
+
+        $scope.pauseSoundcloudTrack = function() { 
+          var widget = SC.Widget(document.querySelector('#soundcloud-div iframe'));
+          widget.pause();
+        }
       });
-      // ;
-    });
+    }  
   }]);
 })();
 

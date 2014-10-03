1e5543e55121ca6535290c4d5a4f381396b03021
Tom <tj.walker@live.com>
2014-10-03 15:14:49 +0100

    css border radius michael fix

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -9,14 +9,16 @@ myApp.setUpEventListeners = function(){
 
     if (!$div_to_rotate.hasClass('rotate')) {
       $div_to_rotate.addClass('rotate');
+      setTimeout(function() {$div_to_rotate.children('.reverse-card').addClass('rounded')}, 500);
       $('.front-card').addClass('avoid-clicks');
       $div_to_show.addClass('show-div');
     }
 
     $('.close-icon').on('click', function(){
       $div_to_rotate.removeClass('rotate');
+      $div_to_rotate.children('.reverse-card').removeClass('rounded');
       $('.front-card').removeClass('avoid-clicks');
-      $div_to_show.removeClass('show-div'); 
+      $div_to_show.removeClass('show-div');
     });
   });
 
@@ -49,26 +51,7 @@ $(document).ready(function(){
   var app = angular.module('finalAppApi', []);
 
   app.controller('appController' , ["$scope", "$http" ,function($scope, $http) {
-
-    hello.on('auth.login', function(auth){
-  
-      // call user information, for the given network
-      hello( auth.network ).api( '/me' ).then( function(r){
-      // Inject it into the container
-        var label = document.getElementById( "profile_"+ auth.network );
-        if(!label){
-          label = document.createElement('div');
-          label.id = "profile_"+auth.network;
-          document.getElementById('profile').appendChild(label);
-        }
-        label.innerHTML = '<img src="'+ r.thumbnail +'" /> Hey '+r.name;
-      });
-    });
-
-    hello.init({ 
-      google : '683586944572-cnol89f6msh0en2b416auj6d3sh7imu3.apps.googleusercontent.com'
-    },{redirect_uri:'http://localhost:3000'});
-        
+      
     $.ajax({
       url: '/',
       dataType: 'json',
@@ -205,7 +188,57 @@ $(document).ready(function(){
   }]);
 })();
 
+// (function ($) {
+//         var _window = { w: window };
 
+//         function adjust_dfs(e) {
+//           var _fsx, _fsy, _fs, _adj, _n;
+        
+//           // get the window dimensions
+//             _window.x = _window.w.innerWidth || _window.e.clientWidth || _window.g.clientWidth;
+//             _window.y = _window.w.innerHeight || _window.e.clientHeight || _window.g.clientHeight;
+
+//           // we get the "base font-size" by dividing into our "core" dimension on 1024x768 and multiplying
+//           // the result by 16 (initial font size for most 
+//           var _fsx = (_window.x / 1024) * 16;
+//             var _fsy = (_window.y / 768) * 16;
+            
+//             // if width > height, then we get the average font size from width and height calculations
+//             // otherwise, if the width of the window is less than the height, we use the width based size
+//             var _fs = (_window.x > _window.y ? ((_fsx + _fsy) * 0.5) : _fsx );
+
+//     // our minimum base font-size should be 8px, or whatever you want
+//             if (_fs < 8) {
+//                 _fs = 8;
+//             }
+
+//     // our maximum base font-size should be 20px, or whatever you want
+//             if (_fs > 20) {
+//                 _fs = 20;
+//             }
+
+    
+//             // we bring the decimal point down to two places, so our performance doesn't take a hit
+//             // when trying to calculate text at a size of 8.1294129836928352903862391em 
+//             _n = parseFloat(_fs, 10).toFixed(2);
+
+//     // setup the css definition object once
+//             _adj = {
+//                 fontSize: _n + 'px',
+//             };
+            
+//     // set the base font size onto our dfs class elements                
+//             $('.dfs').css(_adj);
+
+//         }
+        
+//         // run once on load
+//         adjust_dfs();
+        
+//         // run on window resize
+//         $(window).on('resize', adjust_dfs);
+        
+//     })(jQuery);
 
 
 
